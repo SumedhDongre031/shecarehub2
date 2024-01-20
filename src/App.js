7904292c634ad1afc094a42954bf1c1f68abe83f
Sumedh Dongre <69501694+SumedhDongre031@users.noreply.github.com>
2024-01-20 17:59:14 +0530

    REACT DONE

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import Home from './Home';
+import PreDiagnosisPage from './PreDiagnosisPage';
+import DiagnosisPage from './DiagnosisPage';
+import BiochemicalDiagnosis from './BiochemicalDiagnosis';
+import RadiologicalDiagnosis from './RadiologicalDiagnosis';
+import MedicalImagingModalities from './MedicalImagingModalities';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/pre-diagnosis" element={<PreDiagnosisPage />} />
+        <Route path="/diagnosis" element={<DiagnosisPage />} />
+        <Route path="/biochemical-diagnosis" element={<BiochemicalDiagnosis />} />
+        <Route path="/radiological-diagnosis" element={<RadiologicalDiagnosis />} />
+        <Route path="/medical-imaging-modalities" element={<MedicalImagingModalities />} />
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
